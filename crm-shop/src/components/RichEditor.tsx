2f5aefadbc91ea29f9ce094e1690eee8684035cd
ruoxi <ruoxi@163.com>
2025-12-20 22:27:07 +0800

    修改

--- crm-shop/src/components/RichEditor.tsx
+++ crm-shop/src/components/RichEditor.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useMemo } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
 
 interface RichEditorProps {
@@ -9,6 +9,23 @@ interface RichEditorProps {
 
 const RichEditor: React.FC<RichEditorProps> = ({ value, onChange, height = 500 }) => {
   const editorRef = useRef<any>(null);
+
+  const initConfig = useMemo(() => ({
+    language: 'zh_CN',
+    language_url: '/tinymce/langs/zh_CN.js',
+    height: height,
+    menubar: true,
+    plugins: [
+      'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+      'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+      'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
+    ],
+    toolbar: 'undo redo | blocks | ' +
+      'bold italic forecolor | alignleft aligncenter ' +
+      'alignright alignjustify | bullist numlist outdent indent | ' +
+      'removeformat | help',
+    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+  }), [height]);
 
   return (
     <>
@@ -22,26 +39,12 @@ const RichEditor: React.FC<RichEditorProps> = ({ value, onChange, height = 500 }
         apiKey="cusr1xg6mls93emhkpuw2cp9kpzd6jlot5vek4wpzx38xotz"
         onInit={(_evt, editor) => editorRef.current = editor}
         initialValue={value}
-        value={value}
         onEditorChange={(newValue) => {
           if (onChange) {
             onChange(newValue);
           }
         }}
-        init={{
-          height: height,
-          menubar: true,
-          plugins: [
-            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
-            'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-            'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
-          ],
-          toolbar: 'undo redo | blocks | ' +
-            'bold italic forecolor | alignleft aligncenter ' +
-            'alignright alignjustify | bullist numlist outdent indent | ' +
-            'removeformat | help',
-          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
-        }}
+        init={initConfig}
       />
     </>
   );
